8d8eb29691dc972570b18d9dab232c2b7fcf0e0f
pr3mak <98553592+pr3mak@users.noreply.github.com>
2022-01-27 20:47:22 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
 
       <main>
         <Header title="Welcome to my app!" />
+        <"And it's a front page of this simple app, hence complex app for me">
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
