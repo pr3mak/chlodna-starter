3324f47eb3288ce9c52771fcc98277e8fec4abab
pr3mak <98553592+pr3mak@users.noreply.github.com>
2022-01-29 17:16:00 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>You"'"re at Chlodna</title>
+        <title>You are at Chlodna</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome at Chlodna Street!" />
         <p className="description">
-          Nothing here yet. <code>Work in progress...</code>
+          Nothing here yet.<code>Work in progress...</code>
         </p>
       </main>
 
